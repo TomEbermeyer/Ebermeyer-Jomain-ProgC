a9c4b92fc5f1723359f77879c4787cae3f8810b7
TomEbermeyer <91121678+TomEbermeyer@users.noreply.github.com>
2022-10-04 19:21:18 +0200

    Add files via upload

---
+++ TP3/src/couleurs.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+struct RGBAC {
+    int R;
+    int G;
+    int B;
+    int A;
+    int C;
+};
+
+int main() {
+    srand(time(NULL));    
+    struct RGBAC c0 = {0x1f,0xfa,0x10,0x20,0};
+    struct RGBAC c1 = {0x2f,0xfb,0x11,0x21,0};
+    struct RGBAC c2 = {0x3f,0xfc,0x12,0x22,0};
+    struct RGBAC c3 = {0x4f,0xfd,0x13,0x23,0};
+    struct RGBAC c4 = {0x5f,0xfe,0x14,0x24,0};
+    struct RGBAC c5 = {0x6f,0xff,0x15,0x25,0};
+    struct RGBAC c6 = {0x7f,0xf1,0x16,0x26,0};
+    struct RGBAC c7 = {0x8f,0xf2,0x17,0x27,0};
+    struct RGBAC c8 = {0x9f,0xf3,0x18,0x28,0};
+    struct RGBAC c9 = {0xaf,0xf4,0x19,0x29,0};
+    
+    struct RGBAC couleurs []={c0,c1,c2,c3,c4,c5,c6,c7,c8,c9};
+    int taille = 100;
+    int tableau[4*100];
+    
+
+    //remplir(tableau);
+    int d;
+    int i;
+    for(i = 0;i<400;i=i+4){
+        d = rand()%9; // entre 0 et 9
+        tableau[i] = couleurs[d].R;
+        tableau[i+1] = couleurs[d].G;
+        tableau[i+2] = couleurs[d].B;
+        tableau[i+3] = couleurs[d].A;
+
+        
+        //printf("!!!%d : %d!!!",d,compteur[d]);
+
+        //printf("%x\n",couleurs[0].R);
+        
+
+    }
+    i = 0;
+    int j;
+    for(i = 0;i<400;i=i+4){
+        for(j=0;j<10;j++){
+            if(tableau[i]==couleurs[j].R &tableau[i+1]==couleurs[j].G &tableau[i+2]==couleurs[j].B &tableau[i+3]==couleurs[j].A){
+                couleurs[j].C += 1;
+            }
+
+        }
+
+
+
+
+    } 
+    i = 0;
+    for(int i = 0;i<400;i++){
+        printf("%x, ",tableau[i]);     
+    }
+
+    printf("\n==================\n");
+    i=0;
+    for(int i = 0;i<=9;i++){
+        printf("La couleur : %x %x %x %d est répétée %d fois\n ",couleurs[i].R,couleurs[i].G,couleurs[i].B,couleurs[i].A,couleurs[i].C);     
+    }
+
+    
+
+
+
+return 0;
+}
---
+++ TP3/src/recherche_dichotomique.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main(){
+    srand(time(NULL));
+    int taille = 100;
+    int tableau[taille];
+    int* pt_tb = &tableau[0];
+    int i;
+
+    for(i=0;i<taille;i++){
+        *(pt_tb+i) = rand()%100;
+
+    }
+    i =0;
+    printf("Affichage du tableau non trié : \n");
+    for(i=0;i<taille; i++){
+        printf("%i, ",tableau[i]);
+    }
+
+    int permutation = 1;
+    int c;
+    int j;
+    int v1;
+    int v2;
+    while (permutation != 0){
+        permutation = 0;
+
+        for(i=0;i<taille; i++){
+            v1 = tableau[i];
+            v2 = tableau[i+1];
+            if (v1>v2){
+                tableau[i] = v2;
+                tableau[i+1] = v1;
+                permutation++;
+            }
+        }
+    }
+
+
+    printf("\n");
+    printf("---------------------------------------------\n");
+    printf("Affichage du tableau trié : \n");
+    for(i=0;i<taille; i++){
+        printf("%i, ",tableau[i]);
+    }
+
+    
+
+    int nombre;
+    printf("\n");
+    printf("Entrer le nombre à chercher :");
+    scanf("%d",&nombre);
+
+    //RECHERCHE DICHOTOMETRIE
+  
+    int trouver = 0;
+    int debut = 0;
+    int fin = taille;
+    int milieu = tableau[taille/2];
+
+    //a = (int)4.5 exemple de partie entière 
+
+    while (trouver != 1 & debut<=fin){
+        milieu = (int)((debut+fin)/2);
+        if (tableau[milieu]==nombre){
+            trouver =1;
+        }
+        else{
+            if(nombre >tableau[milieu]){
+                debut = milieu +1;
+            }
+            else{
+                fin = milieu -1;
+            }
+        }
+
+
+    }
+    if (trouver == 1){
+        printf("TOUVER \n");
+    }
+
+
+return 0;
+}
